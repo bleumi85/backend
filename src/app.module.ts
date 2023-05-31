e90e1e2dea8c10a055780b1ce8f177271ff5c768
Jens Bleumer <jens.bleumer@gmail.com>
2023-05-31 08:34:21 +0200

    jbl(): add migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CustomConfigModule } from './config';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroORM } from '@mikro-orm/core';
+import { MikroOrmMiddleware, MikroOrmModule } from '@mikro-orm/nestjs';
 import { AccountsModule } from './api/accounts/accounts.module';
 import { AuthModule } from './api/auth/auth.module';
 import { SeasonsModule } from './api/seasons/seasons.module';
@@ -10,10 +11,28 @@ import { PaymentsModule } from './api/payments/payments.module';
 import { GamedaysModule } from './api/gamedays/gamedays.module';
 import { SeasonGamedaysModule } from './api/season-gamedays/season-gamedays.module';
 import { BetsModule } from './api/bets/bets.module';
+import { AccountSeeder } from './seeders';
 
 @Module({
     imports: [CustomConfigModule, MikroOrmModule.forRoot(), AccountsModule, AuthModule, SeasonsModule, PaymentsModule, GamedaysModule, SeasonGamedaysModule, BetsModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit, OnModuleDestroy, NestModule {
+    constructor(
+        private readonly orm: MikroORM,
+    ) {}
+
+    async onModuleInit() {
+        await this.orm.getMigrator().up();
+        await this.orm.getSeeder().seed(AccountSeeder);
+    }
+
+    async onModuleDestroy() {
+        await this.orm.close();
+    }
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(MikroOrmMiddleware).forRoutes('*');
+    }
+}
---
+++ src/seeders/AccountSeeder.ts
@@ -0,0 +1,36 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import * as bcrypt from 'bcrypt';
+import { Account } from '../api/accounts/entities';
+import { AccountsFactory } from '../api/accounts/accounts.factory';
+import { Role } from '../api/accounts/accounts.interface';
+
+export class AccountSeeder extends Seeder {
+
+  /* eslint-disable @typescript-eslint/no-unused-vars */
+  async run(em: EntityManager): Promise<void> {
+    const countAdmin = await em.count(Account, { role: Role.ADMIN });
+
+    if (countAdmin < 1) {
+      const admin = em.create(Account, {
+        firstName: 'Admin',
+        lastName: 'Api',
+        userName: 'admin',
+        email: 'admin@graftipp.online',
+        role: Role.ADMIN,
+        passwordHash: await bcrypt.hash('Abcd!2345', 10),
+        verified: new Date(Date.UTC(2000, 0, 1)),
+        acceptTerms: true,
+      })
+    }
+
+    let n = Number(process.env.SEED_VISITORS) || 10;
+    const countVisitors = await em.count(Account, { role: Role.VISITOR });
+    n = n - Math.min(countVisitors, n);
+    const accounts = await new AccountsFactory(em).create(n, {
+      verified: new Date(Date.UTC(2000, 0, 1)),
+      acceptTerms: true,
+    })
+  }
+  /* eslint-enable @typescript-eslint/no-unused-vars */
+}
